743f25aa2c51078cea9035099b5cf9ef91cef6b2
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-10-24 17:52:40 +0300

    fixes

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -4,4 +4,6 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test([=[Tests]=] "/home/iwaly/2022_2_Teiko/cmake-build-debug/tests")
+set_tests_properties([=[Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/iwaly/2022_2_Teiko/CMakeLists.txt;31;add_test;/home/iwaly/2022_2_Teiko/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
