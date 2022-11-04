e282db515f12269d4cc08800999bd64307385f85
Keberson <creedkiller.62@gmail.com>
2022-11-04 23:24:28 +0300

    Delete useless directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iwaly/2022_2_Teiko
-# Build directory: /home/iwaly/2022_2_Teiko/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Tests]=] "/home/iwaly/2022_2_Teiko/cmake-build-debug/tests")
-set_tests_properties([=[Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/iwaly/2022_2_Teiko/CMakeLists.txt;31;add_test;/home/iwaly/2022_2_Teiko/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
