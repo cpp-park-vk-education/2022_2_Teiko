96b896f142898274ee89ba3e710f59256cf1d6af
Keberson <creedkiller.62@gmail.com>
2022-11-21 16:39:14 +0300

    Add Base Mock for DBMS

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
 
+#include "../include/dbms.h"
 
+class MockDBMS : public DMBS {
+public:
+    MOCK_METHOD(state, addData, (data_t data), (override));
+    MOCK_METHOD(data_t, getData, (/* Здесь будет идентификатор для получения данных */), (override));
+    MOCK_METHOD(state, findData, (data_t data), (override));
+};
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
